b55c42156055310937609f7f50886bc17989a41e
baosen <chibaosen@gmail.com>
2026-04-23 23:37:18 +0200

    refactor(sdl): drop unused LinkedList import in StateNetCreateRoomSDL

--- src/main/java/mu/nu/nullpo/gui/sdl/StateNetCreateRoomSDL.java
+++ src/main/java/mu/nu/nullpo/gui/sdl/StateNetCreateRoomSDL.java
@@ -9,7 +9,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
