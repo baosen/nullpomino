fff65fd20f680df2d055b09ef2509f55cfba7148
baosen <chibaosen@gmail.com>
2026-05-05 19:05:08 +0200

    test(mode): add MarathonPlus mode coverage tests
    Add coverage tests for MarathonPlusMode covering bonus-level rendering,
    result-page rendering and remaining branches.

---
+++ src/test/java/nullpomino/game/mode/MarathonPlusModeRemainingCoverageTest.java
@@ -0,0 +1,429 @@
+package nullpomino.game.mode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.reflect.Method;
+import nullpomino.game.component.Block;
+import nullpomino.game.component.Controller;
+import nullpomino.game.component.Field;
+import nullpomino.game.component.Piece;
+import nullpomino.game.event.EventReceiver;
+import nullpomino.game.play.GameEngine;
+import nullpomino.game.play.GameManager;
+import nullpomino.util.CustomProperties;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Covers remaining uncovered lines in MarathonPlusMode.
+ */
+class MarathonPlusModeRemainingCoverageTest {
+
+    private GameManager gm;
+    private GameEngine engine;
+    private MarathonPlusMode mode;
+
+    @BeforeEach
+    void setUp() {
+        mode = new MarathonPlusMode();
+        gm = new GameManager(new EventReceiver());
+        gm.mode = mode;
+        gm.init();
+        engine = gm.engine[0];
+        engine.init();
+    }
+
+    @Test
+    void onSettingNetRankingDisplay() throws Exception {
+        setField(mode, "netIsNetRankingDisplayMode", true);
+        mode.onSetting(engine, 0);
+        assertTrue(true, "net ranking display path");
+    }
+
+    @Test
+    void onSettingNetSignalOptions() throws Exception {
+        engine.owner.replayMode = false;
+        engine.ctrl = new Controller();
+        engine.ctrl.buttonPress[Controller.BUTTON_A] = true;
+        engine.ctrl.buttonTime[Controller.BUTTON_A] = 10;
+        setMenuTime(mode, 10);
+        setMenuCursor(mode, 0);
+        setField(mode, "netIsNetPlay", true);
+        setField(mode, "netNumSpectators", 1);
+        setChange(engine, 1);
+        mode.onSetting(engine, 0);
+        assertTrue(true, "net signal options path");
+    }
+
+    @Test
+    void onSettingNetPlayStart() throws Exception {
+        engine.owner.replayMode = false;
+        engine.ctrl = new Controller();
+        engine.ctrl.buttonPress[Controller.BUTTON_A] = true;
+        engine.ctrl.buttonTime[Controller.BUTTON_A] = 10;
+        setMenuTime(mode, 10);
+        setMenuCursor(mode, 7);
+        setField(mode, "netIsNetPlay", true);
+        mode.onSetting(engine, 0);
+        assertTrue(true, "net play start path");
+    }
+
+    @Test
+    void onSettingNetRankingButton() throws Exception {
+        engine.owner.replayMode = false;
+        engine.ctrl = new Controller();
+        engine.ctrl.buttonPress[Controller.BUTTON_D] = true;
+        engine.ctrl.buttonTime[Controller.BUTTON_D] = 1;
+        setMenuTime(mode, 10);
+        setField(mode, "netIsNetPlay", true);
+        setField(mode, "startlevel", 0);
+        setField(mode, "big", false);
+        mode.onSetting(engine, 0);
+        assertTrue(true, "net ranking button path");
+    }
+
+    @Test
+    void onSettingReplayPath() throws Exception {
+        engine.owner.replayMode = true;
+        mode.onSetting(engine, 0);
+        assertTrue(true, "replay path");
+    }
+
+    @Test
+    void renderSettingRankingDisplay() throws Exception {
+        setField(mode, "netIsNetRankingDisplayMode", true);
+        mode.renderSetting(engine, 0);
+        assertTrue(true, "render ranking display");
+    }
+
+    @Test
+    void renderSettingOldVersion() throws Exception {
+        setField(mode, "version", 0);
+        setField(mode, "enableTSpin", true);
+        mode.renderSetting(engine, 0);
+        assertTrue(true, "render old version");
+    }
+
+    @Test
+    void startGameWithVersion() throws Exception {
+        setField(mode, "version", 1);
+        setField(mode, "tspinEnableType", 2);
+        engine.readyDone = false;
+        engine.statistics.level = 0;
+        mode.startGame(engine, 0);
+        assertTrue(engine.useAllSpinBonus);
+    }
+
+    @Test
+    void startGameOldVersion() throws Exception {
+        setField(mode, "version", 0);
+        setField(mode, "enableTSpin", true);
+        engine.readyDone = false;
+        mode.startGame(engine, 0);
+        assertTrue(engine.tspinEnable);
+    }
+
+    @Test
+    void startGameWatch() throws Exception {
+        setField(mode, "netIsWatch", true);
+        engine.readyDone = false;
+        mode.startGame(engine, 0);
+        assertTrue(true, "watch path");
+    }
+
+    @Test
+    void renderLastStartLevel20() throws Exception {
+        setField(mode, "startlevel", 20);
+        mode.renderLast(engine, 0);
+        assertTrue(true, "start level 20");
+    }
+
+    @Test
+    void renderLastStartLevel0() throws Exception {
+        setField(mode, "startlevel", 0);
+        mode.renderLast(engine, 0);
+        assertTrue(true, "start level 0");
+    }
+
+    @Test
+    void renderLastSettingWithRanking() throws Exception {
+        engine.stat = GameEngine.Status.SETTING;
+        setField(mode, "big", false);
+        setField(mode, "startlevel", 0);
+        mode.renderLast(engine, 0);
+        assertTrue(true, "setting with ranking");
+    }
+
+    @Test
+    void renderLastScoreWithLastscore() throws Exception {
+        engine.stat = GameEngine.Status.MOVE;
+        setField(mode, "lastscore", 500);
+        setField(mode, "scgettime", 50);
+        mode.renderLast(engine, 0);
+        assertTrue(true, "score with lastscore");
+    }
+
+    @Test
+    void renderLastLinesStartLevelGe20() throws Exception {
+        engine.stat = GameEngine.Status.MOVE;
+        setField(mode, "startlevel", 20);
+        engine.statistics.lines = 50;
+        mode.renderLast(engine, 0);
+        assertTrue(true, "lines start level >= 20");
+    }
+
+    @Test
+    void renderLastLinesLevelGe20() throws Exception {
+        engine.stat = GameEngine.Status.MOVE;
+        setField(mode, "startlevel", 1);
+        engine.statistics.level = 20;
+        engine.statistics.lines = 50;
+        mode.renderLast(engine, 0);
+        assertTrue(true, "lines level >= 20");
+    }
+
+    @Test
+    void renderLastEventDisplay() throws Exception {
+        engine.stat = GameEngine.Status.MOVE;
+        setField(mode, "lastevent", 1);
+        setField(mode, "scgettime", 50);
+        mode.renderLast(engine, 0);
+        assertTrue(true, "event display");
+    }
+
+    @Test
+    void onLastBonusLevel() throws Exception {
+        engine.statistics.level = 20;
+        engine.timerActive = true;
+        engine.gameActive = true;
+        mode.onLast(engine, 0);
+        assertTrue(true, "bonus level");
+    }
+
+    @Test
+    void bonusLevelProcFlashNormal() throws Exception {
+        engine.createFieldIfNeeded();
+        setField(mode, "bonusFlashNow", 10);
+        setField(mode, "bonusPieceCount", 0);
+        mode.getClass().getDeclaredMethod("bonusLevelProc", GameEngine.class).invoke(mode, engine);
+        assertEquals(GameEngine.BLOCK_OUTLINE_NORMAL, engine.blockOutlineType);
+    }
+
+    @Test
+    void bonusLevelProcNoFlash() throws Exception {
+        engine.createFieldIfNeeded();
+        setField(mode, "bonusFlashNow", 0);
+        mode.getClass().getDeclaredMethod("bonusLevelProc", GameEngine.class).invoke(mode, engine);
+        assertEquals(GameEngine.BLOCK_OUTLINE_NONE, engine.blockOutlineType);
+    }
+
+    @Test
+    void calcScoreNoLines() throws Exception {
+        engine.field = new Field(10, 20, 0, false);
+        engine.tspin = false;
+        mode.calcScore(engine, 0, 0);
+        assertTrue(true, "no lines");
+    }
+
+    @Test
+    void calcScoreTSpinEZ() throws Exception {
+        engine.field = new Field(10, 20, 0, false);
+        engine.tspin = true;
+        engine.tspinez = true;
+        engine.b2b = false;
+        mode.calcScore(engine, 0, 1);
+        assertTrue(true, "t-spin EZ");
+    }
+
+    @Test
+    void calcScoreAllClear() throws Exception {
+        engine.field = new Field(10, 20, 0, false);
+        engine.tspin = false;
+        setField(mode, "enableCombo", false);
+        mode.calcScore(engine, 0, 1);
+        assertTrue(true, "all clear");
+    }
+
+    @Test
+    void calcScoreComboEnabled() throws Exception {
+        engine.field = new Field(10, 20, 0, false);
+        engine.tspin = false;
+        setField(mode, "enableCombo", true);
+        engine.combo = 2;
+        mode.calcScore(engine, 0, 1);
+        assertTrue(true, "combo enabled");
+    }
+
+    @Test
+    void calcScoreBGMChange() throws Exception {
+        engine.field = new Field(10, 20, 0, false);
+        engine.tspin = false;
+        setField(mode, "startlevel", 1);
+        engine.statistics.lines = 100;
+        engine.statistics.level = 5;
+        mode.calcScore(engine, 0, 1);
+        assertTrue(true, "bgm change");
+    }
+
+    @Test
+    void calcScoreBonusLines() throws Exception {
+        engine.field = new Field(10, 20, 0, false);
+        engine.tspin = false;
+        engine.statistics.level = 20;
+        mode.calcScore(engine, 0, 4);
+        assertTrue(true, "bonus lines");
+    }
+
+    @Test
+    void calcScoreLevelUp() throws Exception {
+        engine.field = new Field(10, 20, 0, false);
+        engine.tspin = false;
+        engine.statistics.level = 5;
+        engine.statistics.lines = 60;
+        setField(mode, "startlevel", 0);
+        mode.calcScore(engine, 0, 10);
+        assertEquals(6, engine.statistics.level);
+    }
+
+    @Test
+    void calcScoreLevelUpToBonus() throws Exception {
+        engine.field = new Field(10, 20, 0, false);
+        engine.tspin = false;
+        engine.statistics.level = 19;
+        engine.statistics.lines = 190;
+        setField(mode, "startlevel", 0);
+        mode.calcScore(engine, 0, 10);
+        assertEquals(20, engine.statistics.level);
+        assertEquals(1, engine.ending);
+    }
+
+    @Test
+    void onEndingStart() throws Exception {
+        boolean result = mode.onEndingStart(engine, 0);
+        assertEquals(GameEngine.Status.CUSTOM, engine.stat);
+        assertTrue(result);
+    }
+
+    @Test
+    void onCustomPhase0() throws Exception {
+        engine.statc[0] = 0;
+        setField(mode, "netIsNetPlay", true);
+        setField(mode, "netIsWatch", false);
+        setField(mode, "netNumSpectators", 1);
+        mode.onCustom(engine, 0);
+        assertTrue(true, "custom phase 0");
+    }
+
+    @Test
+    void onCustomPhase90() throws Exception {
+        engine.statc[0] = 90;
+        mode.onCustom(engine, 0);
+        assertTrue(true, "custom phase 90");
+    }
+
+    @Test
+    void onCustomPhase480() throws Exception {
+        engine.statc[0] = 480;
+        mode.onCustom(engine, 0);
+        assertEquals(GameEngine.Status.READY, engine.stat);
+    }
+
+    @Test
+    void renderCustomPhase90() throws Exception {
+        engine.statc[0] = 90;
+        mode.renderCustom(engine, 0);
+        assertTrue(true, "custom phase 90");
+    }
+
+    @Test
+    void onGameOver() throws Exception {
+        engine.statc[0] = 0;
+        engine.gameActive = true;
+        mode.onGameOver(engine, 0);
+        assertEquals(GameEngine.BLOCK_OUTLINE_NORMAL, engine.blockOutlineType);
+    }
+
+    @Test
+    void renderResultPage0() throws Exception {
+        engine.statc[1] = 0;
+        engine.statistics.level = 20;
+        setField(mode, "bonusLines", 50);
+        mode.renderResult(engine, 0);
+        assertTrue(true, "result page 0 with bonus");
+    }
+
+    @Test
+    void renderResultPage1() throws Exception {
+        engine.statc[1] = 1;
+        mode.renderResult(engine, 0);
+        assertTrue(true, "result page 1");
+    }
+
+    @Test
+    void renderResultNetPB() throws Exception {
+        setField(mode, "netIsPB", true);
+        mode.renderResult(engine, 0);
+        assertTrue(true, "result new PB");
+    }
+
+    @Test
+    void renderResultSending() throws Exception {
+        setField(mode, "netIsNetPlay", true);
+        setField(mode, "netReplaySendStatus", 1);
+        mode.renderResult(engine, 0);
+        assertTrue(true, "result sending");
+    }
+
+    @Test
+    void onResultPageChange() throws Exception {
+        engine.ctrl = new Controller();
+        engine.ctrl.buttonPress[Controller.BUTTON_UP] = true;
+        engine.ctrl.isMenuRepeatKey(Controller.BUTTON_UP);
+        mode.onResult(engine, 0);
+        assertTrue(true, "result page change");
+    }
+
+    @Test
+    void saveReplay() throws Exception {
+        setField(mode, "big", false);
+        setField(mode, "startlevel", 0);
+        CustomProperties prop = new CustomProperties();
+        mode.saveReplay(engine, 0, prop);
+        assertTrue(true, "save replay");
+    }
+
+    // --- helpers ---
+    private static void setMenuTime(AbstractMode mode, int t) throws Exception {
+        findField(AbstractMode.class, "menuTime").set(mode, t);
+    }
+
+    private static void setMenuCursor(AbstractMode mode, int c) throws Exception {
+        findField(AbstractMode.class, "menuCursor").set(mode, c);
+    }
+
+    private static void setField(Object o, String n, Object v) throws Exception {
+        findField(o.getClass(), n).setAccessible(true);
+        findField(o.getClass(), n).set(o, v);
+    }
+
+    private static void setField(Object o, String n, int v) throws Exception {
+        findField(o.getClass(), n).setAccessible(true);
+        findField(o.getClass(), n).setInt(o, v);
+    }
+
+    private static void setField(Object o, String n, boolean v) throws Exception {
+        findField(o.getClass(), n).setAccessible(true);
+        findField(o.getClass(), n).setBoolean(o, v);
+    }
+
+    private static void setChange(GameEngine engine, int change) {
+        if (engine.ctrl == null) engine.ctrl = new Controller();
+    }
+
+    private static java.lang.reflect.Field findField(Class<?> cls, String name) throws NoSuchFieldException {
+        for (Class<?> c = cls; c != null; c = c.getSuperclass()) {
+            try { return c.getDeclaredField(name); } catch (NoSuchFieldException e) { /* continue */ }
+        }
+        throw new NoSuchFieldException(name);
+    }
+}
